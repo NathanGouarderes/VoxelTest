70e1db7d250e0cd6ca85609ff82878d9b0d44809
Moi <105416557+NathanGouarderes@users.noreply.github.com>
2026-07-29 16:02:46 +0200

    Add SurfaceWavelength property to FChunckGenJob

--- Source/VoxelModule/Public/FChunckGenJob.h
+++ Source/VoxelModule/Public/FChunckGenJob.h
@@ -17,6 +17,7 @@ struct FChunkGenJob
 	int32 ChunkSize = 128; // Valeur par défaut de sécurité
 	float SurfaceFrequency = 0.01f;
 	float SurfaceAmplitude = 50.0f;
+	float SurfaceWavelength = 100.0f;
 	int32 BaseHeight = 64;
 	float CaveFrequency = 0.01f;
 	float CaveThreshold = 0.5f;
