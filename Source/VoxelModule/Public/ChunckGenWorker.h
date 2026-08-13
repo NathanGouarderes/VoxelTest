17663bd638d3816e8b334ce6492942cef87dbd39
Moi <105416557+NathanGouarderes@users.noreply.github.com>
2026-08-13 15:16:25 +0200

    Add EvaluateNoiseMaterial function declaration

--- Source/VoxelModule/Public/ChunckGenWorker.h
+++ Source/VoxelModule/Public/ChunckGenWorker.h
@@ -26,6 +26,7 @@ public:
 	virtual void Stop() override;
 	static bool EvaluateNoiseSolid(int32 gx, int32 gy, int32 gz, FastNoiseLite& SurfaceNoise, FastNoiseLite& CaveNoise,
 float SurfaceAmplitude, float SurfaceWavelength, int32 BaseHeight, float CaveFrequency, float CaveThreshold);
+	static int8 EvaluateNoiseMaterial(bool bIsSolid);
 
 private:
 	FCriticalSection StopMutex;
