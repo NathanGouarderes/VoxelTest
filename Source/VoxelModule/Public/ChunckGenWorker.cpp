165e8fb9c4c34468ee3bd52e6ced5098e219d618
Moi <105416557+NathanGouarderes@users.noreply.github.com>
2026-08-13 14:00:20 +0200

    Refactor ChunckGenWorker for improved readability

--- Source/VoxelModule/Public/ChunckGenWorker.cpp
+++ Source/VoxelModule/Public/ChunckGenWorker.cpp
@@ -1,110 +1,113 @@
 // Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "ChunckGenWorker.h"
 #include "ChunckManager.h"
 #include "FChunkGenResult.h"
 #include "VoxelWorld.h"
-
 ChunckGenWorker::ChunckGenWorker(AChunckManager* InManager, TQueue<FChunkGenJob, EQueueMode::Mpsc>& InQueue)
-	: bStopRequest(false)
-	, ChunckManager(InManager)
-	, JobQueue(InQueue)
+    : bStopRequest(false)
+    , ChunckManager(InManager)
+    , JobQueue(InQueue)
 {
 }
-
 ChunckGenWorker::~ChunckGenWorker()
 {
 }
-
 bool ChunckGenWorker::Init()
 {
-	return true;
+    return true;
 }
-
 uint32 ChunckGenWorker::Run()
 {
-	while (!bStopRequest)
-	{
-		FChunkGenJob Job;
-		bool bGotJob = false;
-
-		if(ChunckManager)
-		{
-			FScopeLock Lock(&ChunckManager->DequeueMutex);
-			bGotJob = JobQueue.Dequeue(Job);
-		}
-
-		if (!bGotJob)
-		{
-			FPlatformProcess::Sleep(0.01f);
-			continue;
-		}
-
-		const int32 ChunckSize = Job.ChunkSize;
-		const int32 TotalSize = ChunckSize * ChunckSize * ChunckSize;
-
-		TArray<FVoxelDataStructure> LocalVoxel;
-		LocalVoxel.SetNumZeroed(TotalSize);
-
-		for (int x = 0; x < ChunckSize; x++)
-		{
-			for (int y = 0; y < ChunckSize; y++)
-			{
-				float WorldX = (Job.Coord.X * ChunckSize + x) * Job.SurfaceFrequency;
-				float WorldY = (Job.Coord.Y * ChunckSize + y) * Job.SurfaceFrequency;
-
-				float SurfaceNoiseValue = Job.SurfaceNoise.GetNoise(WorldX, WorldY);
-				int GlobalSurfaceHeigh = Job.BaseHeight + FMath::FloorToInt(SurfaceNoiseValue * Job.SurfaceAmplitude);
-
-				for (int z = 0; z < ChunckSize; z++)
-				{
-					int Index = x + y * ChunckSize + z * ChunckSize * ChunckSize;
-					int GlobalZ = Job.Coord.Z * ChunckSize + z;
-
-					bool IsSolid = (GlobalZ < GlobalSurfaceHeigh);
-					if (IsSolid)
-					{
-						float CaveNoiseValue = Job.CaveNoise.GetNoise((Job.Coord.X * ChunckSize + x) * Job.CaveFrequency, (Job.Coord.Y * ChunckSize + y) * Job.CaveFrequency, GlobalZ * Job.CaveFrequency);
-
-						if (CaveNoiseValue > Job.CaveThreshold)
-						{
-							IsSolid = false;
-						}
-					}
-					LocalVoxel[Index].Material.Id = IsSolid ? 1 : 0;
-				}
-			}
-		}
-		bool bIsAllSolid = true;
-		bool bIsAllEmpty = true;
-
-		for (FVoxelDataStructure Voxel : LocalVoxel)
-		{
-			if (Voxel.Material.Id == 0)
-			{
-				bIsAllSolid = false;
-			}
-			else
-			{
-				bIsAllEmpty = false;
-			}
-		}
-		if (ChunckManager)
-		{
-			FChunkGenResult Result;
-			Result.Coord = Job.Coord;
-			Result.Voxels = MoveTemp(LocalVoxel);
-			Result.bIsAllEmpty = bIsAllEmpty;
-			Result.bIsAllSolid = bIsAllSolid;
-			ChunckManager->ChunckGenerationResult.Enqueue(MoveTemp(Result));
-		}
-	}
-	return 0;
+    while (!bStopRequest)
+    {
+        FChunkGenJob Job;
+        bool bGotJob = false;
+        if (ChunckManager)
+        {
+            FScopeLock Lock(&ChunckManager->DequeueMutex);
+            bGotJob = JobQueue.Dequeue(Job);
+        }
+        if (!bGotJob)
+        {
+            FPlatformProcess::Sleep(0.01f);
+            continue;
+        }
+        const int32 ChunckSize = Job.ChunkSize;
+        const int32 SubSize = ChunckSize >> Job.LOD;
+        const int32 Step = 1 << Job.LOD;
+        const int32 TotalSize = SubSize * SubSize * SubSize;
+        TArray<FVoxelDataStructure> LocalVoxel;
+        LocalVoxel.SetNumZeroed(TotalSize);
+        bool bIsAllSolid = true;
+        bool bIsAllEmpty = true;
+        for (int x = 0; x < SubSize; x++)
+        {
+            for (int y = 0; y < SubSize; y++)
+            {
+                float WorldX = (Job.Coord.X * ChunckSize + x * Step);
+                float WorldY = (Job.Coord.Y * ChunckSize + y * Step);
+                float SurfaceNoiseValue = Job.SurfaceNoise.GetNoise(WorldX / Job.SurfaceWavelength, WorldY / Job.SurfaceWavelength);
+                int GlobalSurfaceHeigh = Job.BaseHeight + FMath::FloorToInt(SurfaceNoiseValue * Job.SurfaceAmplitude);
+                for (int z = 0; z < SubSize; z++)
+                {
+                    int Index = x + y * SubSize + z * SubSize * SubSize;
+                    int GlobalZ = Job.Coord.Z * ChunckSize + z * Step;
+                    bool IsSolid = (GlobalZ < GlobalSurfaceHeigh);
+                    if (IsSolid)
+                    {
+                        float CaveNoiseValue = Job.CaveNoise.GetNoise((Job.Coord.X * ChunckSize + x * Step) * Job.CaveFrequency, (Job.Coord.Y * ChunckSize + y * Step) * Job.CaveFrequency, GlobalZ * Job.CaveFrequency);
+                        if (CaveNoiseValue > Job.CaveThreshold)
+                        {
+                            IsSolid = false;
+                        }
+                    }
+                    LocalVoxel[Index].Material.Id = IsSolid ? 1 : 0;
+                    if (IsSolid)
+                    {
+                        bIsAllEmpty = false;
+                    }
+                    else
+                    {
+                        bIsAllSolid = false;
+                    }
+                }
+            }
+        }
+        if (ChunckManager)
+        {
+            FChunkGenResult Result;
+            Result.Coord = Job.Coord;
+            Result.LOD = Job.LOD;
+            Result.GenerationId = Job.GenerationId;
+            Result.Voxels = MoveTemp(LocalVoxel);
+            Result.bIsAllEmpty = bIsAllEmpty;
+            Result.bIsAllSolid = bIsAllSolid;
+            ChunckManager->ChunckGenerationResult.Enqueue(MoveTemp(Result));
+        }
+    }
+    return 0;
 }
-
 void ChunckGenWorker::Stop()
 {
-	FScopeLock Lock(&StopMutex);
-	bStopRequest = true;
+    FScopeLock Lock(&StopMutex);
+    bStopRequest = true;
+}
+
+
+bool ChunckGenWorker::EvaluateNoiseSolid(int32 gx, int32 gy, int32 gz, FastNoiseLite SurfaceNoise, FastNoiseLite CaveNoise,
+float SurfaceAmplitude, float SurfaceWavelength, float SurfaceAmplitude, int32 BaseHeight, float CaveFrequency, float CaveThreshold)
+{
+	float SurfaceAmplitude = SurfaceNoise.GetNoise(gx / SurfaceWavelength, gy / SurfaceWavelength);
+	int SurfaceHeight = BaseHeight + FMath::FloorToInt(SurfaceNoiseValue * SurfaceAmplitude);
+
+	if(gz >= SurfaceHeight)
+	{
+		return false;
+	}
+	float CaveNoiseValue = CaveNoise.GetNoise(gx  * Job.CaveFrequency, gy * CaveFrequency, gz * CaveFrequency);
+	if(CaveNoiseValue > CaveThreshold)
+	{
+		return false;
+	}
+	return true;
 }
